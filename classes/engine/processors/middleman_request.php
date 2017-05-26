5c29153f7f11b7ea5185a30b4fbcd21a84335bd1
Marko <contact.pavlovic@gmail.com>
2017-05-26 20:05:49 +0000

    Use http helper in middleman_request.php

--- classes/engine/processors/middleman_request.php
+++ classes/engine/processors/middleman_request.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace local_imtt\engine\processors;
+use local_imtt\http;
 
 class middleman_request extends base {
 
@@ -38,22 +39,7 @@ class middleman_request extends base {
     }
 
     public function send_request($url, $request_data) {
-        $request_data_json = json_encode($request_data);
-        $curl = curl_init($url);
-
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $request_data_json);
-
-        $response_data_json = curl_exec($curl);
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        curl_close($curl);
-
-        $response_data = json_decode($response_data_json, true);
-        return array('status' => $status, 'data' => $response_data);
+        return http\helper::send_json_request($url, $request_data);
     }
 }
 
