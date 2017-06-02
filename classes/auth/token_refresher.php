1b39ecf2e9ff278fadd79413d900e9106d7ae1ec
Marko <contact.pavlovic@gmail.com>
2017-06-02 09:11:49 +0000

    Hotfix token_refresher refresh_google_token method

--- classes/auth/token_refresher.php
+++ classes/auth/token_refresher.php
@@ -19,12 +19,20 @@ class token_refresher {
     }
 
     public function refresh_google_token() {
+        $tokens = json_decode($this->access_token, true);
+        $refresh_token = $tokens['refresh_token'];
+
         $client = get_google_client();
+        $client->setClientId(get_config('googledocs', 'clientid'));
+        $client->setClientSecret(get_config('googledocs', 'secret'));
         $client->setAccessToken($this->access_token);
-        if ($client->isAccessTokenExpired()) $client->refreshToken();
-        $tokens = json_decode($client->getAccessToken(), true);
-        $tokens['refresh_token'] = $client->getRefreshToken();
-        return json_encode($tokens);
+
+        if ($client->isAccessTokenExpired())
+            $client->refreshToken($refresh_token);
+
+        $new_tokens = json_decode($client->getAccessToken(), true);
+        $new_tokens['refresh_token'] = $client->getRefreshToken();
+        return json_encode($new_tokens);
     }
 }
 
