7da5ad580e38159b799af04eeec58bb65bd70bae
Marko <contact.pavlovic@gmail.com>
2017-06-06 09:40:06 +0000

    Refactor database structure

--- db/upgrade.php
+++ db/upgrade.php
@@ -59,8 +59,78 @@ function xmldb_local_imtt_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2017051000, 'local', 'imtt');
     }
 
-    if ($oldversion < 2017052700) {
-        upgrade_plugin_savepoint(true, 2017052700, 'local', 'imtt');
+    if ($oldversion < 2017060600) {
+        //===============================
+        // create local_imtt_token table
+        //===============================
+
+        // Define table local_imtt_token to be created.
+        $table = new xmldb_table('local_imtt_token');
+
+        // Adding fields to table local_imtt_token.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('provider_name', XMLDB_TYPE_CHAR, '64', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('provider_access_token', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table local_imtt_token.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for local_imtt_token.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        //=========================
+        // update local_imtt table
+        //=========================
+
+        $table = new xmldb_table('local_imtt');
+
+        // Define field provider_access_token to be dropped from local_imtt.
+        $field = new xmldb_field('provider_access_token');
+
+        // Conditionally launch drop field provider_access_token.
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Define field provider_name to be dropped from local_imtt.
+        $field = new xmldb_field('provider_name');
+
+        // Conditionally launch drop field provider_name.
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Define field event_names to be dropped from local_imtt.
+        $field = new xmldb_field('event_names');
+
+        // Conditionally launch drop field event_names.
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Define field token_id to be added to local_imtt.
+        $field = new xmldb_field('token_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, 'configuration_json');
+
+        // Conditionally launch add field token_id.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        upgrade_plugin_savepoint(true, 2017060600, 'local', 'imtt');
+    }
+
+    if ($oldversion < 2017060601) {
+        // Define key token_id (foreign) to be added to local_imtt.
+        $table = new xmldb_table('local_imtt');
+        $key = new xmldb_key('token_id', XMLDB_KEY_FOREIGN, array('token_id'), 'local_imtt_token', array('id'));
+
+        // Launch add key token_id.
+        $dbman->add_key($table, $key);
+
+        // Imtt savepoint reached.
+        upgrade_plugin_savepoint(true, 2017060601, 'local', 'imtt');
     }
 }
 ?>
