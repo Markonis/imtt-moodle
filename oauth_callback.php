43225dd8d4e3d5bf458db069ca75c73ff4e74d24
Marko <contact.pavlovic@gmail.com>
2017-06-06 10:07:54 +0000

    Create imtt_token instead of imtt_instance in oauth_callback.php

--- oauth_callback.php
+++ oauth_callback.php
@@ -3,6 +3,9 @@
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/google/lib.php');
 
+use local_imtt\auth;
+use local_imtt\model;
+
 require_login();
 
 $course_id = required_param('course_id', PARAM_INT); // Course ID
@@ -12,15 +15,15 @@ $url = null;
 
 if ($code) {
     // Get Google access token
-    $google_auth = new \local_imtt\auth\google_auth($course_id);
+    $google_auth = new auth\google_auth($course_id);
     $google_auth->client->authenticate($code);
 
     $tokens = json_decode($google_auth->client->getAccessToken(), true);
     $tokens['refresh_token'] = $google_auth->client->getRefreshToken();
 
     // Create an IMTT instance for this course with the Google access token
-    $result = \local_imtt\model\imtt_instance::create($DB, array(
-        'course_id' => $course_id,
+    $result = model\imtt_token::create($DB, array(
+        'user_id' => $USER->id,
         'provider_name' => 'google',
         'provider_access_token' => json_encode($tokens)));
 
